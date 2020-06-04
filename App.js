beca0b5f74c3212ddd19a2c90b2271c12c7c2a12
Costas-Zeimpekis <morpheustudio@gmail.com>
2020-06-04 21:09:07 +0300

    fix: add Image and Touchable

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Button } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  TouchableHighlight,
+  Button,
+  Image,
+  View
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      <View style={{ marginBottom: 15 }}>
+        <TouchableHighlight onPress={() => console.log("Press Image")}>
+          <Image
+            source={{
+              width: 200,
+              height: 300,
+              uri: "https://picsum.photos/200/300"
+            }}
+          />
+        </TouchableHighlight>
+      </View>
       <Button color="orange" onPress={() => alert("Hello")} title="Click Me" />
     </SafeAreaView>
   );
